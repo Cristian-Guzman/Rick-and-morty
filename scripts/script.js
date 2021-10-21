f7dafb1a77d0e291fce5e7b77d29d7a504289c6e
Juan Pablo Bedoya Vasco <bedoyavascojuanpablo@gmail.com>
2021-10-21 16:55:33 -0500

    Se crea cards en Javascript

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,8 @@ async function getApi(url){
 try{
     const res = await fetch(URL);
     const data = await res.json();
-    // console.log(data.results);
+    console.log(data.results);
+    
     showCartel();
 }catch(error){
     console.log("Error en la ejecucion " + error);
@@ -13,12 +14,24 @@ try{
 getApi(URL);
 
 function showCartel (data){
-    
     const main = document.getElementById('main');
     main.innerHTML= ``;
     const chapter = document.createElement('div');
+    chapter.classList.add('content');
     chapter.innerHTML = `
-               
+    <div class="chapters-grid">
+        <div class="chapters-card">
+            <img src="" alt="uno">
+            <h2>Beta-Seven</h2>
+            <h3>Alive - Alien</h3>
+            <h4> Last known location:</h4>
+            <h3>unknown</h3>
+            First seen in:
+            Auto Erotic Assimilation
+        </div>
+        
+
     `
+
     main.appendChild(chapter);
 };
